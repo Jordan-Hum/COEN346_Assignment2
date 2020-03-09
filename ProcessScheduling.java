0f7e49f133c6991b741e8ab0090ab5c0d0f0c91e
JordanHum <jhum268@gmail.com>
2020-03-08 23:31:43 -0400

    got the readFile() working

--- ProcessScheduling.java
+++ ProcessScheduling.java
@@ -1,53 +1,84 @@
 //Jordan Hum - 40095876 | Anthony Iacampo - 40096683
+import java.io.*;
 import java.util.*;
 
 class ProcessScheduling {
   boolean hasCPU;
-  public static void main(String args[]) {
-    //read Input.txt file
+  static int arrivalTime[];
+  static int execTime[];
+  public static void main(String args[]) throws Exception 
+  {
+    //Read Input.txt file
     readFile();
 
+    printArray(arrivalTime);
+    System.out.println();
+    printArray(execTime);
   }
 
-    //Read the input file
-    private static void readFile() throws Exception 
+  //Read the input file
+  private static void readFile() throws Exception 
+  {
+    File file = new File("Input.txt");
+    List<String> inputList = new ArrayList<String>();
+    
+    try 
     {
-      FileReader fr = new FileReader("Input.txt");
-      BufferedReader br = new BufferedReader(fr);
-      
-      String input;
-      int argsIndex = 0;
-      
-      numBulbs = Integer.parseInt(br.readLine());
-      bulbs = new int[numBulbs];
-      defectiveBulbs = new int[numBulbs];
-      while((input = br.readLine()) != null)
+      Scanner reader = new Scanner(file);
+      while (reader.hasNext()) 
       {
-        if(Integer.parseInt(input) == 0 || Integer.parseInt(input) == 1)
-        {
-        try
-        {
-        bulbs[argsIndex] = Integer.parseInt(input);
-          argsIndex++;
-        }
-        catch(Exception err)
-        {
-          System.out.println("Invalid array size");
-            System.exit(0);
-        }
-        }
-        else
-        {
+        String input = reader.next();
+        inputList.add(input);
+      }
+      reader.close();
+    } 
+    catch (Exception e) 
+    {
+      System.out.println(e);
+    }
+
+    //Set size for arrays
+    int arraySize = inputList.size()/2;
+    arrivalTime = new int[arraySize];
+    execTime = new int[arraySize];
+
+    //All even indexes for process number
+    int processSetCounter = 0;
+    for(int i=0; i < inputList.size(); i += 2)
+    {
+      try 
+      {
+        arrivalTime[processSetCounter++] = Integer.parseInt(inputList.get(i));
+      } 
+      catch (Exception e) 
+      {
         System.out.println("Invalid input");
         System.exit(0);
-        }
-      }
-      if(numBulbs > argsIndex)
-      {
-      System.out.println("Invalid array size");
-        System.exit(0);
-      }
-      br.close();
+      } 
     }
-  
+
+    //All odd indexes for execution time
+    int timeSetCounter = 0;
+    for(int i=1; i < inputList.size(); i += 2)
+    {
+      try
+      {
+        execTime[timeSetCounter++] = Integer.parseInt(inputList.get(i));
+      }
+      catch (Exception e) 
+      {
+        System.out.println("Invalid input");
+        System.exit(0);
+      } 
+    }
+  }
+
+   //Prints the array
+   private static void printArray(int[] array)
+   {
+     for(int i = 0; i < array.length; i++)
+     {
+         System.out.print(array[i]);
+     }
+   }
 }
